70642d7fa7d4ddea1eac94809ed1b66f3f1e44e0
j0s3b0 <38701651+j0s3b0@users.noreply.github.com>
2018-05-29 10:29:30 -0400

    Update test.js

--- jenkins/script/test.js
+++ jenkins/script/test.js
@@ -1,6 +1,6 @@
 const login = require('../src/index');
 
 test("The registered student must be registered", () => {
-  expect(login("ss")).toBeTruthy();
+  expect(login("jesus")).toBeTruthy();
   //toBeTruthy
 });
